908968a60c6ecaa843029a35ce7d52fa7b60a6f1
WannaMinPaing <paingwanna@gmail.com>
2024-10-09 11:56:45 +0630

    change from loginController to authcontroller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class LoginController extends Controller
+class AuthController extends Controller
 {
     use ResponseHelper;
 
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(LoginController::class)->group(function () {
+Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [LoginController::class, 'logout']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
